ec81794809736b88411fa6217e99f7d4d378f106
Joseph O'Gorman <joe@meganmeiessmbp4.home>
2023-10-27 13:15:24 -0400

    updated fix on settings.py - allowed_hosts

--- gasworks/settings.py
+++ gasworks/settings.py
@@ -32,7 +32,7 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 # Application definition
