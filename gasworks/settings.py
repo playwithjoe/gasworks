48414e9382ea4a354cd8591d6f7e2d6aff8c3c35
Joseph O'Gorman <joe@meganmeiessmbp4.home>
2023-10-30 22:41:57 -0400

    added database credentials

--- gasworks/settings.py
+++ gasworks/settings.py
@@ -82,27 +82,29 @@ WSGI_APPLICATION = 'gasworks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 '''
+
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "Hvacservice01",
-        "HOST": "service-db.cp4uvyw8uohp.ca-central-1.rds.amazonaws.com",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
