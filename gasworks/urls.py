bba812deff2c12840668bab1a05103577ce5f0ee
Joseph O'Gorman <joe@meganmeiessmbp4.home>
2023-10-30 22:16:20 -0400

    adding database

--- gasworks/urls.py
+++ gasworks/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("service/", include("service.urls")),
+    path("", include("service.urls")),
     path('admin/', admin.site.urls),
 ]
